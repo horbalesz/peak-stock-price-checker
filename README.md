8bcde7583b7949164c12d2b677731ab9ce6558a8
horbalesz <horbalesz97@gmail.com>
2024-05-09 13:20:53 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# peak-stock-price-checker
-The application should periodically check the prices and calculate the moving average of stocks, using Finnhub's data.
+# Stock Price Checker
+
+The application should periodically check the prices and calculate the moving average of stocks, using Finnhub's data. 
+
+## How to run
+
+1. Create .env file 
+
+    This file must contain the following variables to work:
+    ```
+    DATABASE_USERNAME
+    DATABASE_PASSWORD
+    DATABASE_HOST
+    DATABASE_NAME
+    FINNHUB_API_KEY
+    ```
+2. Run `docker compose up -d`
+
+    `docker logs -f --tail 100 {database|webapp}` with this command you can see a rolling log for either the `database` or the `webapp` service
+
---
+++ src/app.module.ts
@@ -0,0 +1,34 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { StockController } from './stock/stock.controller';
+import { StockModule } from './stock/stock.module';
+
+@Module({
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true
+        }),
+        TypeOrmModule.forRootAsync({
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => {
+                return {
+                    type: 'mysql',
+                    host: configService.get('DATABASE_HOST'),
+                    port: 3306,
+                    username: configService.get('DATABASE_USERNAME'),
+                    password: configService.get('DATABASE_PASSWORD'),
+                    database: configService.get('DATABASE_NAME'),
+                    entities: [
+                        __dirname + '/entities/*.entity{.ts,.js}',
+                    ],
+                    synchronize: true,
+                }
+            }
+        }),
+        StockModule,
+
+    ],
+    controllers: [StockController]
+})
+export class AppModule { }
